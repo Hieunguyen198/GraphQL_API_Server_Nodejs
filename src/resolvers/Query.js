f3da4d71e203ae0ec2b520a3ee1dd3669cfbe2b6
Hieu Nguyen <hieuisme98@gmail.com>
2021-03-30 13:49:29 +0700

    fm

---
+++ src/resolvers/Query.js
@@ -0,0 +1,49 @@
+function feed(parent, args, context, info) {
+  const where =  args.filter
+  ?{
+    OR:[
+      {description:{contains: args.filter}},
+      {url:{contains: args.filter}}
+    ]
+  }
+  :
+  {}
+    const links= context.prisma.link.findMany({ 
+      where,
+      skip: args.skip,
+      take: args.take,
+      orderBy: args.orderBy,
+    })
+    return links
+}
+
+function user(parent,args,context,info){
+  const where = args.filter 
+  ?{
+    OR:[
+      {name:{contains: args.filter}},
+      {email:{contains: args.filter}}
+    ]
+  }
+  :{}
+  const users = context.prisma.user.findMany({where,})
+  return users
+}
+
+function vote(parent,args,context,info){
+  const where = args.filter
+  ?{
+    OR:[
+      {name: {contains: args.filter}},
+      {email: {contains: args.filter}}
+    ]
+  }:{}
+  const votes= context.prisma.vote.findMany({where,})
+  return votes
+}
+
+module.exports = {
+    feed,
+    user,
+    vote,
+}
---
+++ src/resolvers/Subscription.js
@@ -0,0 +1,37 @@
+function newLinkSubscribe(parent, args, context, info) {
+    return context.pubsub.asyncIterator("NEW_LINK")
+  }
+  
+const newLink = {
+    subscribe: newLinkSubscribe,
+    resolve: payload => {
+      return payload
+    },
+}
+
+function newVoteSubscribe(parent, args, context, info) {
+    return context.pubsub.asyncIterator("NEW_VOTE")
+}
+  
+const newVote = {
+    subscribe: newVoteSubscribe,
+    resolve: payload => {
+      return payload
+    },
+}
+function newUserSubscribe(parent,args,context,info){
+  return context.pubsub.asyncIterator("NEW_USER")
+}
+  
+const newUser = {
+  subscribe: newUserSubscribe,
+  resolve:payload =>{
+    return payload
+  },
+}
+
+module.exports = {
+    newLink,
+    newVote,
+    newUser,
+}
